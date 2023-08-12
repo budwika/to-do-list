154edc348798e3a89340cb887fd48c2b53a488f8
budwika <95086386+budwika@users.noreply.github.com>
2023-08-12 12:18:21 +0530

    Making interface.ts and few files

---
+++ src/Interfaces.ts
@@ -0,0 +1,3 @@
+export interface ITask{
+    taskName:String;
+}
---
+++ src/components/TodoTask.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoTask = () => {
+    return (
+        <div> Task </div>
+    )
+}
+
+export default TodoTask

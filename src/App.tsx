7ee68b9a866ad1d522fc2c0609c8111d121372af
budwika <95086386+budwika@users.noreply.github.com>
2023-08-12 12:17:48 +0530

    Making interface.ts and few files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
+import{ITask} from './Interfaces';
+import TodoTask from './components/TodoTask';
+
 
 const App = () => {
 
 const [task,setTask] = useState<string>("")
-const [todo,setTodoList] = useState([]);
+const [todoList,setTodoList] = useState<ITask[]>([]);
+
+
 
 const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
   /*if (event.target.name=="task"){
@@ -15,15 +20,25 @@ const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
   setTask(event.target.value)
 };
 
+const addTask = (): void => {
+  const newTask = {taskName: task};
+  setTodoList([...todoList, newTask])
+  setTask("")
+}
+
   return (
     <div className="App">
     <div className='header'>
       <div className='inputContainer'>
-        <input type='text' placeholder='Task...' name="task" onChange={handleChange}/>
+        <input type='text' placeholder='Task...' name="task" value={task} onChange={handleChange}/>
       </div>
-    <button>Add Task</button>
+    <button onClick={addTask}>Add Task</button>
     </div>
-    <div className='todoList'></div>
+    <div className='todoList'>
+      {todoList.map(() => {
+        return <TodoTask/>;
+      })}
+    </div>
     </div>
   );
 }
